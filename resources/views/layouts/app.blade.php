f87bbcc295f5db49eef7c0681843b04db6cf0e26
QuentinEkm <quentinekm@gmail.com>
2022-11-04 09:33:14 +0100

    adding changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,13 +46,13 @@
 </head>
 <body>
 @yield('content')
-<script src="{{asset ("") }}"></script>
-<script src="{{asset ("") }}"></script>
-<script src="{{asset ("") }}"></script>
-<script src="{{asset ("") }}"></script>
-<script src="{{asset ("") }}"></script>
-<script src="{{asset ("") }}"></script>
-<script src="{{asset ("") }}"></script>
-<script src="{{asset ("") }}"></script>
+{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> --}}
+    <script src={{ asset("assets/js/bootstrap.min.js") }}></script>
+    <script src={{ asset("assets/js/jquery.slicknav.min.js") }}></script>
+    <script src={{ asset("assets/js/slick.min.js") }}></script>
+    <script src={{ asset("assets/js/touchswipe.min.js") }}></script>
+    <script src={{ asset("assets/js/skrollr.min.js") }}></script>
+    <script src={{ asset("assets/js/jquery.countTo.min.js") }}></script>
+    <script src={{ asset("assets/js/script.js") }}></script>
 </body>
 </html>
